ea0700bdc1180f52128ce4851b4af678fd28a95d
Adol Rodriguez <adolsalamanca@gmail.com>
2023-12-30 11:47:46 +0100

    fix corner cases

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,7 +4,7 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    if let Ok(lines) = read_lines("./test.txt") {
+    if let Ok(lines) = read_lines("./input.txt") {
         let mut out:u32=0;
         for result in lines {
             if let Ok(line) = result {
@@ -50,33 +50,47 @@ fn find_first_and_last_digit_second_part(input: &str) -> u32 {
     let mut number_found = false;
     let mut first_digit: u32 = 0;
     let mut last_digit: u32 = 0;
-
-    let re = Regex::new(r"one|two|three|four|five|six|seven|eight|nine|ten|\d").unwrap();
+    // Overlapping numbers: twone, oneight, threeight, sevenine, fiveight, eighthree, eightwo, nineight
+    let re = Regex::new(r"twone|oneight|threeight|fiveight|sevenine|eighthree|eightwo|nineight|one|two|three|four|five|six|seven|eight|nine|ten|\d").unwrap();
     let matches : Vec<&str> = re.find_iter(input).map(|m| m.as_str()).collect();
 
     for i in matches {
         if !number_found {
-            first_digit = parse_as_number(i);
+            first_digit = parse_first_number(i);
             number_found = true;
         }
 
-        last_digit = parse_as_number(i);
+        last_digit = parse_last_number(i);
     }
 
     first_digit*10 + last_digit
 }
 
-fn parse_as_number(p0: &str) -> u32 {
+fn parse_first_number(p0: &str) -> u32 {
     return match p0 {
-        "one" | "1" => { 1 }
-        "two" | "2" => { 2 }
-        "three" | "3" => { 3 }
+        "one" | "oneight"| "1" => { 1 }
+        "two" | "twone" | "2" => { 2 }
+        "three" | "threeight" | "3" => { 3 }
+        "four" | "4" => { 4 }
+        "five" | "fiveight" | "5" => { 5 }
+        "six" | "6" => { 6 }
+        "seven" | "sevenine" | "7" => { 7 }
+        "eight" | "eighthree" | "eightwo" | "8" => { 8 }
+        "nine" | "nineight" | "9" => { 9 }
+        _ => { 0 }
+    }
+}
+fn parse_last_number(p0: &str) -> u32 {
+    return match p0 {
+        "one" | "twone" | "1" => { 1 }
+        "two" | "eightwo" | "2" => { 2 }
+        "three" | "eighthree" |"3" => { 3 }
         "four" | "4" => { 4 }
         "five" | "5" => { 5 }
         "six" | "6" => { 6 }
         "seven" | "7" => { 7 }
-        "eight" | "8" => { 8 }
-        "nine" | "9" => { 9 }
+        "eight" | "oneight" | "threeight" | "fiveight" | "nineight"| "8" => { 8 }
+        "nine" | "sevenine" | "9" => { 9 }
         _ => { 0 }
     }
 }
