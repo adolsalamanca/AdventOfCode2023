15ab9683a7994473bae5ba4aba23bea153467213
Adol Rodriguez <adolsalamanca@gmail.com>
2024-04-27 14:08:07 +0200

    day 4 part b working

--- day4/src/engine.rs
+++ day4/src/engine.rs
@@ -1,4 +1,3 @@
-use std::ops::Deref;
 use std::vec;
 
 #[derive(Clone)]
@@ -35,20 +34,47 @@ impl Game {
         Self { scratch_numbers: scratch_numbers_u32 , winning_numbers: winning_numbers_u32}
     }
 
-    pub fn calculate_points(self) -> u32 {
-
-        let matched_numbers : Vec<&u32> = self.winning_numbers.iter().filter(|&x| {
+    pub fn collect_winning_numbers(&self) -> Vec<&u32> {
+        self.winning_numbers.iter().filter(|&x| {
             if self.scratch_numbers.contains(&x) {
                 return true
             }
             return false
-        }).collect();
+        }).collect()
+    }
 
+    pub fn calculate_points(self) -> u32 {
+        let matched_numbers : Vec<&u32> = self.collect_winning_numbers();
         if matched_numbers.len() == 0 {
             return 0;
         }
 
         u32::pow(2, (matched_numbers.len() - 1)  as u32)
+    }
+
+    pub fn calculate_scratch_cards(self) -> u32 {
+        let winning_numbers: Vec<&u32> = self.collect_winning_numbers();
+
+        if winning_numbers.len() == 0 {
+            return 1;
+        }
+
+        (winning_numbers.len() + 1) as u32
+    }
+
+    pub fn update_scratch_cards_iteration(self, mut iteration: usize, multipliers: &mut Vec<u32>, res: u32) {
+        let multipliers_len = multipliers.clone().len()-1;
+
+        // Repeat the count iteration n times, given that there might be several instances of each card
+        for _ in 0.. multipliers[iteration] {
+            for n_time in 0..res - 1 {
+                let next_index = iteration + n_time as usize + 1;
+                if next_index > multipliers_len {
+                    continue
+                }
+                multipliers[next_index]+=1;
+            }
+        }
     }
 }
 
@@ -62,8 +88,40 @@ mod tests {
         let input_str: &str = "\
 Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 ";
-        let mut game = Game::new(input_str);
+        let game = Game::new(input_str);
 
         assert_eq!(8, game.calculate_points());
+    }
+
+    #[test]
+    fn should_calculate_scratch_cards_from_scratchcard() {
+        let input_str: &str = "\
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+";
+
+        let mut iteration:usize = 0;
+        let mut total_cards:u32 = 0;
+
+        let mut multipliers:Vec<u32> = vec![1; input_str.lines().count()];
+
+        // I'm putting logic in tests, this is not nice.
+        input_str.lines().for_each(|line| {
+            let game = Game::new(line);
+
+            let res = game.clone().calculate_scratch_cards();
+            game.update_scratch_cards_iteration(iteration, &mut multipliers, res);
+            iteration += 1;
+        });
+
+        multipliers.iter().for_each(|m| {
+            total_cards += m;
+        });
+
+        assert_eq!(30, total_cards);
     }
 }
--- day4/src/main.rs
+++ day4/src/main.rs
@@ -7,15 +7,23 @@ use crate::engine::Game;
 
 fn main() {
     if let Ok(lines) = read_lines("./input.txt") {
-        let mut points :u32 = 0;
-        lines.flatten().for_each(|line| {
+        let mut iteration:usize = 0;
+        let mut total_cards:u32 = 0;
+
+        let collected_lines :Vec<String> = lines.flatten().collect();
+        let mut multipliers:Vec<u32> = vec![1; collected_lines.clone().len()];
+
+        collected_lines.iter().for_each(|line| {
             let game = Game::new(line.as_str());
-            let p : u32 = game.calculate_points();
-            // TODO, fix failure in card 22 or 23, there's a panic.
-            points += p;
+            let res = game.clone().calculate_scratch_cards();
+            game.update_scratch_cards_iteration(iteration, &mut multipliers, res);
+            iteration += 1;
         });
 
-        println!("The result is: {}", points);
+        multipliers.iter().for_each(|m| {
+            total_cards += m;
+        });
+        println!("The result is: {}", total_cards);
     }
 }
 
